0fdda5d86201828cba2b816600325bea770af895
Piotr Duda <piotrduda1992@gmail.com>
2021-06-16 16:26:41 +0200

    Arrows for tabs

--- src/pages/Redstone/Tokens/Tokens.vue
+++ src/pages/Redstone/Tokens/Tokens.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="token-tabs">
-    <b-tabs sm-pills md-tabs nav-class="bg-transparent">
-      <b-tab v-for="type in tokenTypes" :key="type.label">
-        <template #title>
-          {{type.label}} <span class="tokens-number">{{ filteredTokenWithPrices(prices, type.tag).length }}</span>
-        </template>
-        <TokenCards :tokens="filteredTokenWithPrices(prices, type.tag)" />
-      </b-tab>
-    </b-tabs>
+  <div class="token-tabs" ref="tabWrapper" :class="{visible: showTabs}">
+      <div id="leftArr" class="arrow" @click="scrollLeft()" v-if="showArrows">
+        <i class="fa fa-angle-left"></i>
+      </div>
+      <div id="rightArr" class="arrow" @click="scrollRight()" v-if="showArrows">
+        <i class="fa fa-angle-right"></i>
+      </div>
+      <b-tabs sm-pills md-tabs nav-class="bg-transparent" ref="tabScroll" :class="[{showArrows}]">                
+        <b-tab v-for="type in tokenTypes" :key="type.label">
+          <template #title>
+            {{type.label}} <span class="tokens-number">{{ filteredTokenWithPrices(prices, type.tag).length }}</span>
+          </template>
+          <TokenCards :tokens="filteredTokenWithPrices(prices, type.tag)" />
+        </b-tab>
+      </b-tabs> 
   </div>
 </template>
 
@@ -84,7 +90,10 @@ export default {
   data() {
     return {
       prices: {},
-      tokenTypes: TOKEN_TYPES
+      tokenTypes: TOKEN_TYPES,
+      back: false,
+      showArrows: false,
+      showTabs: false
     };
   },
 
@@ -95,6 +104,8 @@ export default {
   },
 
   async mounted() {
+    setTimeout(() => this.resize(), 0);
+
     if (currentPrices) {
       this.prices = currentPrices;
     } else {
@@ -102,10 +113,21 @@ export default {
       this.prices = currentPrices;
     }
   },
+
+  created() {
+    window.addEventListener("resize", this.resize);
+    this.showTabs = true;
+    this.resize();
+  },
+
+  destroyed() {
+    window.removeEventListener("resize", this.resize);
+  },
   
   methods: {
     filteredTokenWithPrices(fetchedPrices, type) {
       const result = [];
+
       for (const symbol of Object.keys(tokensData)) {
         const token = tokensData[symbol];
         let shouldBeAdded = true;
@@ -142,26 +164,68 @@ export default {
           );
         }
       }
-
       return result;
     },
+    scrollLeft() {
+      let content = document.querySelector(".nav-tabs");
+      content.scrollBy({ 
+        left: -200, 
+        behavior: 'smooth' 
+      });
+    },
+    scrollRight() {
+      let content = document.querySelector(".nav-tabs");
+      content.scrollBy({ 
+        left: 200, 
+        behavior: 'smooth' 
+      });
+    },
+    isOverflowing() {
+      let tabWrapper =  this.$refs.tabWrapper;
+      let tabs = [...this.$refs.tabScroll.$el.getElementsByTagName("li")];
+
+      let tabsWidth = 0;
+
+      tabs.forEach(
+        tab => {
+          tabsWidth += tab.offsetWidth
+        }
+      )
+
+      tabs.reduce( (a, b) => {
+        return a + b.offsetWidth;
+      }, 0)
+
+      return tabWrapper.offsetWidth < tabsWidth
+    },
+    resize() {
+      this.showArrows = this.isOverflowing();
+    }
   },
 }
 </script>
 
 <style src="./Tokens.scss" lang="scss" scoped />
 <style lang="scss">
+@import '../../../styles/app';
+
 //scrollable tabs
 .token-tabs {
+  visibility: hidden;
+  &.visible {
+    visibility: visible;
+  }
+
   .nav-tabs {
     flex-wrap: nowrap;
     white-space: nowrap;
     overflow-y: clip;
     overflow-x: scroll;
+  }
 
-    @media (max-width: breakpoint-max(sm)) {
-      max-width: 500px;
-    }
+  .showArrows .nav-tabs {
+    margin-left: 30px;
+    max-width: calc(100% - 58px);
   }
 
   a {
@@ -177,6 +241,30 @@ export default {
 
   ::-webkit-scrollbar {
     display: none;
+  }
+
+  .arrow {
+    padding: 10px;
+    position: absolute;
+    z-index: 2
+  } 
+
+  #leftArr {
+    left: 40px;
+  }
+
+  #rightArr {
+    right: 40px;
+  }
+
+  @media (max-width: breakpoint-max(sm)) {
+    #leftArr {
+      left: 20px;
+    }
+
+    #rightArr {
+      right: 20px;
+    }
   }
 }
 </style>
