8f6b3ecdbddd4413b56e5881d1da1778f71defc1
hatskier <suvorovalex1997@gmail.com>
2021-09-20 18:36:36 +0200

    added sources table

--- src/pages/Redstone/Source/Source.vue
+++ src/pages/Redstone/Source/Source.vue
@@ -5,10 +5,6 @@
       {{ sourceId }}
     </h1>
 
-    <div>
-      {{ report }}
-    </div>
-
     <div class="chart-container">
       <SourceChartsContainer :stats="sourceStats" />
     </div>
@@ -26,77 +22,26 @@ import axios from "axios";
 import SourceChartsContainer from "@/components/Source/SourceChartsContainer";
 import sources from "redstone-node/dist/src/config/sources.json";
 
-const exampleReport = {
-  "fetching-failed": {
-    "2021-08-26": 26,
-    "2021-08-25": 52,
-    "2021-08-24": 30,
-    "2021-08-23": 18
-  },
-  "symbol-not-in-reponse": {
-    "2021-08-25": 142
-  },
-  "incorrect-price-value": {
-    "2021-08-26": 1078,
-    "2021-08-25": 2808,
-    "2021-08-24": 2840,
-    "2021-08-23": 1740
-  },
-  "symbols": {
-    "not-in-response": [
-      "LRC",
-      "ENJ",
-      "SOL",
-      "CHZ",
-      "BSV",
-      "SAND",
-      "ZIL",
-      "KSM",
-      "AXS",
-      "SHIB",
-      "UMA",
-      "VET",
-      "ZEN",
-      "THETA",
-      "SRM",
-      "REP",
-      "HT",
-      "FTT",
-      "LPT",
-      "OKB",
-      "AKRO",
-      "REEF",
-      "CVC",
-      "CAKE",
-      "XVS",
-      "CEL",
-      "PAXG",
-      "AUDIO",
-      "SOC",
-      "DOCK",
-      "RAY",
-      "TFUEL",
-      "STEP"
-    ],
-    "incorrect-value": [
-      "BSV"
-    ]
-  }
-};
+const SOURCE_REPORT_URL = "https://raw.githubusercontent.com/redstone-finance/redstone-reports/main/last/sources-report.json";
 
 export default {
   name: "Source",
 
   data() {
     return {
-      exampleReport,
       report: null,
     };
   },
 
   async mounted() {
-    const response = await axios.get("https://raw.githubusercontent.com/redstone-finance/redstone-reports/main/last/sources-report.json");
-    this.report = response.data;
+    await this.loadDataForSoruce();
+  },
+
+  methods: {
+    async loadDataForSoruce() {
+      const response = await axios.get(SOURCE_REPORT_URL);
+      this.report = response.data;
+    },
   },
 
   computed: {
--- src/pages/Redstone/Sources/Sources.vue
+++ src/pages/Redstone/Sources/Sources.vue
@@ -1,51 +1,160 @@
 <template>
   <div class="sources-wrapper">
-    <div class="mb-lg">
-      <b-row>
-        <b-col
-          xxl="3" xl="4" lg="6" md="12" sm="12" xs="12"
-          v-for="(source, index) in sources" :key="index"
-        >
-          <div class="mb-4" @click="$router.push('/app/source/' + source.id)">
-            <Widget class="mb-0 source-card">
-              <div class="source-details">
-                <div class="source-logo">
-                  <img :src="source.logoURI" :alt="source.id + '-logo'" />
-                </div>
-                <div class="source-name">
-                  {{ source.id }}
-                </div>
-                <div class="see-more">
-                  See more
-                  <i class="fa fa-angle-right" />
-                </div>
-              </div>
-            </Widget>
-          </div>
-          
-        </b-col>
-      </b-row>
+    <b-table
+      id="sources-table"
+      stacked="md"
+      sort-icon-left
+      :busy="loading"
+      hover
+      :items="sources"
+      :fields="fields"
+      >
+
+      <template #cell(name)="source">
+        <div class="source-name">
+          <img class="source-logo" :src="source.item.logoURI" />
+          <span class="ml-3">
+            {{ source.item.id }}
+          </span>
+        </div> 
+      </template>
+
+      <template #cell(fetching-success-percentage)="source">
+        <span
+          class="stability-percantage"
+          :style="{color: getColorForPercentage(source.item['fetching-success-percentage'])}">
+          {{ source.item['fetching-success-percentage'] }}%
+        </span>
+      </template>
+
+      <template #cell(link)="source">
+        <a :href="source.item.url" target="_blank">
+          <i class="fa fa-external-link" />
+        </a>
+      </template>
+
+      <template #cell(detailed-report)="source">
+        <div class="source-report-link-container">
+          <a :href="'/#/app/source/' + source.item.id">
+            <span>View report</span>
+            <i class="fa fa-angle-right ml-2" />
+          </a>
+        </div>
+      </template>
+    </b-table>
+
+    <div v-if="showReportsLink" class="report-source-link">
+      Data source:
+      <a target="_blank" href="https://github.com/redstone-finance/redstone-reports">
+      github.com/redstone-finance/redstone-reports
+      </a>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import _ from "lodash";
 import sources from "redstone-node/dist/src/config/sources.json";
+
+const SOURCE_REPORT_URL = "https://raw.githubusercontent.com/redstone-finance/redstone-reports/main/last/sources-report.json";
+const MAX_FETCHING_SUCCESS = 60 * 24 * 5; // 5 days of the main redstone-node work
+
+function getFetchingSuccessPercentage(failCount) {
+  return Number((100 * (MAX_FETCHING_SUCCESS - failCount) / MAX_FETCHING_SUCCESS).toFixed(2));
+}
 
 export default {
   data() {
-    return {};
+    return {
+      loading: false,
+      showReportsLink: false,
+      sourcesReportFromGH: {},
+      fields: [
+        { key: 'name', label: 'Source', stickyColumn: true},
+        { key: 'link', label: 'URL'},
+        { key: 'incorrect-price-value', label: 'Incorrect price', sortable: true },
+        { key: 'fetching-failed', label: 'Fetching failed', sortable: true },
+        { key: 'fetching-success-percentage', label: 'Success', sortable: true },
+        { key: 'detailed-report', label: 'Stability report'},
+      ],
+    };
+  },
+
+  async mounted() {
+    await this.loadSourcesReport();
+  },
+
+  methods: {
+    // TODO: add errors handling and notifications
+    async loadSourcesReport() {
+      try {
+        this.loading = true
+        const response = await axios.get(SOURCE_REPORT_URL);
+        this.sourcesReportFromGH = response.data;
+      } finally {
+        this.loading = false
+      }
+    },
+
+    getColorForPercentage(value) {
+      if (value == 100) {
+        return '#0F9D58'; // green
+      } else if (value >= 99) {
+        return '#ff8c00'; // orange
+      } else {
+        return '#DB4437'; // red
+      }
+    },
+
+    getReportForSource(source) {
+      const reportWithDefaultValue = (defaultVal) => ({
+        'incorrect-price-value': defaultVal,
+        'fetching-failed': defaultVal,
+      });
+
+      const getErrCount = (errType) => {
+        const errors = this.sourcesReportFromGH[source][errType];
+        if (!errors) {
+          return 0;
+        } else {
+          const sumFromLogs = _.sum(Object.values(errors).map(count => Number(count)));
+          return sumFromLogs / 2; // Aws cloudwatch counts errors twice
+        }
+      };
+
+      if (this.loading) {
+        return reportWithDefaultValue('...');
+      } else if (this.sourcesReportFromGH && this.sourcesReportFromGH[source]) {
+        return {
+          'incorrect-price-value': getErrCount('incorrect-price-value'),
+          'fetching-failed': getErrCount('fetching-failed'),
+        };
+      } else {
+        return reportWithDefaultValue(0);
+      }
+    }
   },
 
   computed: {
     sources() {
-      return Object.entries(sources).map(([id, details]) => ({
-        id,
-        ...details,
-      }));
+      const result = [];
+      for (const [id, details] of Object.entries(sources)) {
+        const sourceReport = this.getReportForSource(id);
+        const failsCount = sourceReport['fetching-failed'];
+        const successPercentage = getFetchingSuccessPercentage(failsCount);
+        result.push({
+          ...details,
+          id,
+          ...sourceReport,
+          'fetching-success-percentage': successPercentage,
+          stability: 'OK',
+        });
+      }
+      return result;
     },
   }
 }
 </script>
 
-<style lang="scss" src="./Sources.scss" scoped />
+<style lang="scss" src="./Sources.scss" />
