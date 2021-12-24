32d7ef5c9a7a452a1bc6faed8959e7cf513b053b
asiaziola <ziola.jm@gmail.com>
2021-12-24 09:57:14 +0100

    Revert "adding: spinner in the table, pagination-nav linked to the router, gateway url update"
    This reverts commit 1959eb8609e2c1f6ac3bcf4bfb00334ce7f9cc38.

--- src/pages/Redstone/Provider/Provider.vue
+++ src/pages/Redstone/Provider/Provider.vue
@@ -1,27 +1,24 @@
 <template>
-    <div class="provider d-flex flex-column">
-        <div
-            class="d-flex align-items-center mb-3"
-            v-if="provider && provider.profile"
-        >
-            <img class="provider-logo" :src="provider.profile.imgUrl" />
-            <div class="provider-name ml-3">{{ provider.profile.name }}</div>
-        </div>
-        <div v-else class="preloaders">
-            <div class="preloader logo-preloader"></div>
-            <div class="preloader text-preloader"></div>
-        </div>
-        <div class="provider-tabs">
-            <b-tabs>
-                <b-tab title="Details">
-                    <ProviderDetails :provider="provider" />
-                </b-tab>
-                <b-tab title="Manifests">
-                    <Manifests :provider="provider" :providerId="providerId" />
-                </b-tab>
-            </b-tabs>
-        </div>
+  <div class="provider d-flex flex-column">
+    <div class="d-flex align-items-center mb-3" v-if="provider && provider.profile">
+      <img class="provider-logo" :src="provider.profile.imgUrl" />
+      <div class="provider-name ml-3">{{ provider.profile.name }}</div>
     </div>
+    <div v-else class="preloaders">
+      <div class="preloader logo-preloader"></div>
+      <div class="preloader text-preloader"></div>
+    </div>
+    <div class="provider-tabs">
+      <b-tabs>
+        <b-tab title="Details">
+          <ProviderDetails :provider="provider"/>
+        </b-tab>
+        <b-tab title="Manifests">
+          <Manifests :provider="provider" :providerId="providerId" />
+        </b-tab>
+      </b-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,47 +27,51 @@ import Manifests from '@/components/Provider/Manifests';
 import { mapState } from 'vuex';
 
 export default {
-    name: 'Provider',
+  name: "Provider",
 
-    data() {
-        return {
-            selectedManifest: {},
-            fetching: true,
-        };
+  data() {
+    return {
+      selectedManifest: {},
+      fetching: true
+    }; 
+  },
+
+  created() {
+
+  },
+
+  methods: {
+  },
+
+  components: {
+    ProviderDetails,
+    Manifests  
+  },
+
+  computed: {
+    providerId() {
+      return this.$route.params.id;
     },
-
-    created() {},
-
-    methods: {},
-
-    components: {
-        ProviderDetails,
-        Manifests,
-    },
-
-    computed: {
-        providerId() {
-            return this.$route.params.id;
-        },
-        ...mapState('prefetch', {
-            providers: (state) => state.providers,
-        }),
-        provider() {
-            return this.providers ? this.providers[this.providerId] : null;
-        },
-    },
-};
+    ...mapState("prefetch", {
+      providers: (state) => state.providers,
+    }),
+    provider() {
+      return this.providers ? this.providers[this.providerId] : null;
+    }
+  },
+}
 </script>
 
 <style src="./Provider.scss" lang="scss" scoped />
-<style lang="scss">
+<style lang="scss"  >
 .provider-tabs > .tabs > div:first-of-type {
-    height: 44px;
+   height: 44px; 
 }
 
 .provider-tabs {
-    .nav-tabs > .nav-item {
-        flex: 0 0 124px;
-    }
+  .nav-tabs > .nav-item {
+    flex: 0 0 124px;
+  }
 }
 </style>
+
