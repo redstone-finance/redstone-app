8abf0e6ebf9ad663862c86f0932cf1c0d57fc935
Daniel Szerszeń <daniel.szerszen@frompolandwithdev.com>
2024-07-03 16:43:40 +0200

    Improve logic

--- src/store/layers.js
+++ src/store/layers.js
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers'
 import axios from 'axios';
+import { stat } from 'fs';
 
 const ETHER_SCAN_API_KEY = "F13KVG286SK73T1WYNP1WWJW3C3JQFPEUI"
 const LAYERS_SCHEMA_URL = "https://p6s64pjzub.execute-api.eu-west-1.amazonaws.com/dev/execute";
@@ -7,8 +8,13 @@ const LAYERS_SCHEMA_URL = "https://p6s64pjzub.execute-api.eu-west-1.amazonaws.co
 const CONTRACTS_ABI_DEFINITION = [
     "function getBlockTimestampFromLatestUpdate() public view returns (uint256 blockTimestamp)",
     "function getValueForDataFeed(bytes32 dataFeedId) external view returns (uint256)",
+    "function getValuesForDataFeeds(bytes32[] memory requestedDataFeedIds) external view returns (uint256[] memory)",
     "function getDataFeedId() external view returns (bytes32)",
+    "function getDataFeedIds() external view returns (bytes32[] memory)"
 ];
+
+// Helper methods for handling promises
+const etherNetLinkMessage = (address) => `validate it here: https://etherscan.io/address/${address}`
 
 export default {
     namespaced: true,
@@ -29,10 +35,6 @@ export default {
             state.smartContracts[layerId] = contract;
         },
         assignLayerDetails(state, { key, layerId, data }) {
-            if (state.layersDetails[layerId] == null) {
-                // Create empty object only on init of store
-                state.layersDetails[layerId] = {}
-            }
             state.layersDetails[layerId][key] = data
             console.log(state.layersDetails)
         }
@@ -42,6 +44,9 @@ export default {
             const contract = state.smartContracts[layerId]
             if (contract == null) throw Error('No contract assigned to layer id:', layerId)
             return contract
+        },
+        hasMultipleFeeds: (state) => (layerId) => {
+            return state.layersSchema[layerId].priceFeeds.length > 1
         }
     },
     actions: {
@@ -59,43 +64,78 @@ export default {
         },
         // 
         // Handling smart contracts
+        // Condider making some sort of handler for async then/catch/finally cause it hurts eyes. Would be nice to have some sort of generic handler since, methods are very similar
+        // But only when we are sure the business logic + it will be much easier to provide unit tests
         // 
-        async fetchDataFeedId({ commit }, layerId) {
+        async fetchDataFeedId({ commit, state }, layerId) {
             return this.getters['layers/getSmartContractByLayerId'](layerId).getDataFeedId().then(feedId => {
                 commit('assignLayerDetails', { key: 'feedId', layerId, data: feedId })
             }).catch(() => {
-                console.log('No FeedId found for', layerId)
+                console.log(`No FeedId found for ${layerId}, ${etherNetLinkMessage(state.layersSchema[layerId].adapterContract)}`)
+            }).finally(() => {
+                this.dispatch('layers/disableLoader', { layerId, loaderId: 'feedId' })
             })
         },
-        async fetchBlockTimeStamp({ commit }, layerId) {
+        async fetchBlockTimeStamp({ commit, state}, layerId) {
             this.getters['layers/getSmartContractByLayerId'](layerId).getBlockTimestampFromLatestUpdate().then(timestamp => {
                 commit('assignLayerDetails', { key: 'blockTimestamp', layerId, data: timestamp })
             }).catch(() => {
-                console.log('No blockTimestamp found for', layerId)
+                console.log(`No blockTimestamp found for ${layerId}, ${etherNetLinkMessage(state.layersSchema[layerId].adapterContract)}`)
+            }).finally(() => {
+                this.dispatch('layers/disableLoader', { layerId, loaderId: 'timestamp' })
             })
         },
-        async fetchValueForDataFeed({ commit }, { layerId, feedId }) {
+        async fetchValueForDataFeed({ commit, state }, { layerId, feedId }) {
             if (feedId == null) return
-            this.getters['layers/getSmartContractByLayerId'](layerId).getValueForDataFeed(feedId).then(dataFeed => {
+            // Most contract with multiple feeds expose only method for multiple values
+            // I'm not sure if this is a general rule for every contract, would be nice to have consistensy across contracts.
+            const method = this.getters['layers/hasMultipleFeeds'](layerId) ? 'getValuesForDataFeeds' : 'getValueForDataFeed'
+            this.getters['layers/getSmartContractByLayerId'](layerId)[method](feedId).then(dataFeed => {
                 commit('assignLayerDetails', { key: 'dataFeed', layerId, data: dataFeed })
             }).catch(() => {
-                console.log('No dataFeed found for', layerId)
+                // since data feed id is required for fetching feed value we disable the feed value loader as well
+                this.dispatch('layers/fetchValueForDataFeed', { layerId, feedId })
+                console.log(`No dataFeed found for ${layerId}, validate it here: https://etherscan.io/address/${state.layersSchema[layerId].adapterContract}`)
+            }).finally(() => {
+                this.dispatch('layers/disableLoader', { layerId, loaderId: 'value' })
             })
         },
-
-        async fetchFeedIdAndValue({ commit, state }, { layerId }) {
-            this.dispatch('layers/fetchDataFeedId', layerId).then(() => {
+        async fetchFeedIdAndValue({ state }, { layerId }) {
+            const method = this.getters['layers/hasMultipleFeeds'](layerId) ? 'layers/fetchDataFeedIds' : 'layers/fetchDataFeedId'
+            this.dispatch(method, layerId).then(() => {
                 const feedId = state.layersDetails[layerId]?.feedId
-                console.log({feedId})
                 this.dispatch('layers/fetchValueForDataFeed', { layerId, feedId })
             })
         },
         // 
+        // Helpers
+        // 
+        disableLoader({ state }, { layerId, loaderId }) {
+            state.layersDetails[layerId].loaders[loaderId] = false
+        },
+        initializeLayerDetails({ state }) {
+            Object.keys(state.layersSchema).forEach(layerId => {
+                if (state.layersDetails[layerId] == null) {
+                    // Create empty object only on init of store
+                    state.layersDetails[layerId] = {
+                        // additional loader object for each property we fetch so we can reflect it in the ui
+                        // mutation is not really need
+                        loaders: {
+                            feedId: true,
+                            value: true,
+                            timestamp: true,
+                        }
+                    }
+                }
+            });
+        },
+        // 
         // Init fetchning of all details required for the UI
         // 
-        async fetchLayersSchema({ commit }) {
+        async fetchLayersSchema({ commit, state }) {
             const { data } = await axios.get(LAYERS_SCHEMA_URL)
             commit('assignLayerSchema', data)
+            this.dispatch('layers/initializeLayerDetails')
         },
         async initLayersContracts() {
             // await this.fetchDataFeedIds()
