5b27caa8ef48bc79b0fcca2afb116c96023a38f5
Daniel Szerszeń <szerszen.daniel@gmail.com>
2024-08-05 18:29:09 +0200

    [RDS-333] - migrate node to 20x (#85)
    * Update packages
    
    * Add wokring vue config for node 20.x
    
    * Bump version to 2.2.0

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,41 @@
+const path = require('path');
+
 let publicPath = process.env.NODE_ENV === 'production' ? './' : './';
 
 module.exports = {
   publicPath,
   productionSourceMap: true,
   chainWebpack: config => {
-      config.module
-          .rule('vue')
-          .use('vue-loader')
-          .tap(options => {
-              options.compiler = require('vue-template-babel-compiler')
-              return options
-          })
+    // Existing vue-loader configuration
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options.compiler = require('vue-template-babel-compiler')
+        return options
+      });
+
+    // Configuration for handling node_modules, excluding core-js
+    config.module
+      .rule('babel-for-node-modules')
+      .test(/\.m?js$/)
+      .include
+        .add(path.resolve(__dirname, 'node_modules'))
+        .end()
+      .exclude
+        .add(/core-js/)
+        .end()
+      .use('babel-loader')
+        .loader('babel-loader')
+        .options({
+          presets: [
+            ['@babel/preset-env', { modules: 'auto' }]
+          ],
+          plugins: [
+            '@babel/plugin-proposal-private-methods',
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-transform-modules-commonjs'
+          ]
+        });
   }
 }
