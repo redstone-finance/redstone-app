28454369a71b2d1747220946f50449a9133fdd13
Daniel Szerszeń <szerszen.daniel@gmail.com>
2024-08-06 11:20:39 +0200

    Add limitations for pickers

--- src/components/Token/TokenPriceTableContainer.vue
+++ src/components/Token/TokenPriceTableContainer.vue
@@ -10,6 +10,7 @@
             <div class="datepicker-container">
               <label class="mt-2 mt-md-0" for="from-datepicker">Show services from: </label>
               <b-datepicker id="from-datepicker" v-model="fromDate" :value-as-date="true" locale="en-GB"
+                :max="toDate"
                 :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }">
               </b-datepicker>
               <b-form-timepicker v-model="fromTime" locale="en" no-close-button></b-form-timepicker>
@@ -21,6 +22,8 @@
             <div class="datepicker-container">
               <label class="mt-2 mt-md-0" for="to-datepicker">to:</label>
               <b-datepicker id="to-datepicker" v-model="toDate" :value-as-date="true" locale="en-GB"
+                :min="fromDate"
+                :max="new Date()"
                 :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }">
               </b-datepicker>
               <b-form-timepicker v-model="toTime" locale="en" no-close-button></b-form-timepicker>
