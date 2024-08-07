f0ccf491e26f4c67325d62e043ab5734d82d8226
Daniel Szerszeń <szerszen.daniel@gmail.com>
2024-08-07 14:51:57 +0200

    working eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,5 @@ module.exports = {
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'prettier/prettier': 'error'
     }
   }
