ef52a29fef83404e0e66130f0097dbf5bdc80375
Daniel Szerszeń <szerszen.daniel@gmail.com>
2024-08-13 08:49:24 +0200

    Remove config from package

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
     ],
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-unused-vars': 'off'
     }
   }
+
+  
