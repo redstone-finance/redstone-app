ba1bfabf2d98a42e77baed25e371d536ea3d7204
Daniel Szerszeń <szerszen.daniel@gmail.com>
2024-09-17 15:27:47 +0200

    Working new api

--- src/redstone-api-adapter.js
+++ src/redstone-api-adapter.js
@@ -7,6 +7,72 @@ const COINGECKO_URL = "https://dy9r79395zjk.cloudfront.net/api/v3";
 
 const coingeckoId = (symbol) => symbolToId[symbol] || symbol.toLowerCase();
 
+const daysParams = (daysCount) => {
+  const endDate = new Date().getTime();
+  return {
+    toTimestamp: endDate,
+    fromTimestamp: endDate - daysCount * 24 * 3600 * 1000,
+    interval: 3600 * 1000,
+  };
+};
+
+const lastHourParams = () => {
+  const endDate = new Date().getTime();
+  return {
+    toTimestamp: endDate,
+    fromTimestamp: endDate - 1 * 60 * 60 * 1000,
+    interval: 60 * 1000,
+  };
+};
+
+const makeConfigurableRequest = async (baseUrl, params = {}) => {
+  const url = new URL(baseUrl);
+  Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined) {
+      url.searchParams.append(key, value);
+    }
+  });
+  const result = await axios.get(url.toString());
+  console.log({ result });
+  return result.data;
+};
+
+const getApiData = async function (symbol, opts) {
+  const baseUrl = "http://localhost:9000/prices";
+  let params = {
+    provider: opts.provider,
+    symbol: symbol,
+    forceInflux: true,
+    fromTimestamp: opts.startDate,
+    toTimestamp: opts.endDate,
+    interval: 1,
+  };
+  if (opts.daysCount) {
+    params = {
+      ...params,
+      ...daysParams(opts.daysCount),
+    };
+  }
+
+  if (opts.lastHour) {
+    params = {
+      ...params,
+      ...lastHourParams(),
+    };
+  }
+
+  if (opts.symbols) {
+    console.log('add symbols')
+    params = {
+      ...params,
+      symbols: opts.symbols,
+      symbol: undefined
+    };
+  }
+
+  return await makeConfigurableRequest(baseUrl, params);
+};
+
 /** Represents PriceData from redstone-api */
 const priceData = (symbol, provider, value, timestamp) => ({
   symbol,
@@ -29,11 +95,19 @@ function fixHistoricalRedstoneProvider(opts) {
 
 export default {
   getPrice: async function (symbol, opts) {
+    console.log("single price");
     // console.log({api: this.getApiData(symbol, opts)})
     if (opts.provider === "coingecko") {
       const geckoId = coingeckoId(symbol);
-      const url = `${COINGECKO_URL}/simple/price?ids=${geckoId}&vs_currencies=${CURRENCY}&precision=2&include_last_updated_at=true`;
-      const geckoResult = (await axios.get(url)).data;
+      const baseUrl = `${COINGECKO_URL}/simple/price`;
+      const params = {
+        ids: geckoId,
+        vs_currencies: CURRENCY,
+        precision: 2,
+        include_last_updated_at: true,
+      };
+
+      const geckoResult = await makeConfigurableRequest(baseUrl, params);
 
       return priceData(
         symbol,
@@ -42,60 +116,72 @@ export default {
         geckoResult[geckoId]["last_updated_at"] * 1000
       );
     } else {
-      return await redstone
-        .query()
-        .symbol(symbol)
-        .latest()
-        .exec({ defaultProvider: opts.provider });
+      const data = await getApiData(symbol, { provider: opts.provider, symbols: [symbol] });
+      return data[symbol]
+      // return await redstone
+      //   .query()
+      //   .symbol(symbol)
+      //   .latest()
+      //   .exec({ defaultProvider: opts.provider });
     }
   },
-  getApiData: async function (symbol, opts) {
-    const url = `http://localhost:9000/prices?provider=${opts.provider}&symbol=${symbol}&forceInflux=true&fromTimestamp=${opts.startDate}&toTimestamp=${opts.endDate}&interval=1`;
-    const result = (await axios.get(url)).data;
-
-    return result
-  },
   getHistoricalPrice: async function (symbol, opts) {
-    console.log({api: this.getApiData(symbol, opts)})
+    console.log("last hour");
     if (opts.provider === "coingecko") {
-      const from = (opts.startDate / 1000) | 0;
-      const to = (opts.endDate / 1000) | 0;
-      const url = `${COINGECKO_URL}/coins/${coingeckoId(
+      const baseUrl = `${COINGECKO_URL}/coins/${coingeckoId(
         symbol
-      )}/market_chart/range?from=${from}&to=${to}&precision=2&vs_currency=${CURRENCY}`;
-      const geckoResult = (await axios.get(url)).data;
+      )}/market_chart/range`;
+      const params = {
+        from: Math.floor(opts.startDate / 1000),
+        to: Math.floor(opts.endDate / 1000),
+        precision: 2,
+        vs_currency: CURRENCY,
+      };
+
+      const geckoResult = await makeConfigurableRequest(baseUrl, params);
 
       return geckoResult.prices
         .reverse()
         .map((p) => priceData(symbol, opts.provider, p[1], p[0]));
     } else {
-      return (
-        await redstone.getHistoricalPrice(
-          symbol,
-          fixHistoricalRedstoneProvider(opts)
-        )
-      ).map((priceData) => {
-        priceData.provider = opts.provider;
-        return priceData;
+      return await getApiData(symbol, {
+        provider: opts.provider,
+        lastHour: true,
       });
+      // return (
+      //   await redstone.getHistoricalPrice(
+      //     symbol,
+      //     fixHistoricalRedstoneProvider(opts)
+      //   )
+      // ).map((priceData) => {
+      //   priceData.provider = opts.provider;
+      //   return priceData;
+      // });
     }
   },
   query: async function (provider, symbol, daysCount) {
     if (provider === "coingecko") {
-      const url = `${COINGECKO_URL}/coins/${coingeckoId(
+      const baseUrl = `${COINGECKO_URL}/coins/${coingeckoId(
         symbol
-      )}/market_chart?days=${daysCount}&precision=2&vs_currency=${CURRENCY}`;
-      const geckoResult = (await axios.get(url)).data;
+      )}/market_chart`;
+      const params = {
+        days: daysCount,
+        precision: 2,
+        vs_currency: CURRENCY,
+      };
+
+      const geckoResult = await makeConfigurableRequest(baseUrl, params);
 
       return geckoResult.prices
         .reverse()
         .map((p) => priceData(symbol, provider, p[1], p[0]));
     } else {
-      return await redstone
-        .query()
-        .symbol(symbol)
-        .forLastDays(daysCount)
-        .exec({ defaultProvider: provider });
+      return await getApiData(symbol, { provider, daysCount });
+      // return await redstone
+      //   .query()
+      //   .symbol(symbol)
+      //   .forLastDays(daysCount)
+      //   .exec({ defaultProvider: provider });
     }
   },
   getAllPrices: async function (opts) {
