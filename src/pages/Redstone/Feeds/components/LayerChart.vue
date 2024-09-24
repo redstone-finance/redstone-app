7d6da3b0b00288ee52c15aa8cd2127dcc2b22bd8
Daniel Szerszeń <szerszen.daniel@gmail.com>
2024-09-24 13:28:55 +0200

    Fix for mobile

--- src/pages/Redstone/Feeds/components/LayerChart.vue
+++ src/pages/Redstone/Feeds/components/LayerChart.vue
@@ -18,342 +18,352 @@
 </template>
 
 <script>
-  import Chart from "chart.js";
-  import { isValid, startOfDay, subDays, subMonths, parseISO } from "date-fns";
-  import { currencySymbolMap } from "./../utils/FeedsTableDataLayer";
-  const crosshairPlugin = {
-    afterDatasetsDraw: function (chart) {
-      if (chart.tooltip._active && chart.tooltip._active.length) {
-        var activePoint = chart.tooltip._active[0],
-          ctx = chart.ctx,
-          x = activePoint.tooltipPosition().x,
-          y = activePoint.tooltipPosition().y,
-          topY = chart.scales["y-axis-0"].top,
-          bottomY = chart.scales["y-axis-0"].bottom,
-          leftX = chart.scales["x-axis-0"].left,
-          rightX = chart.scales["x-axis-0"].right;
+import Chart from "chart.js";
+import { isValid, subDays, subMonths, parseISO } from "date-fns";
+import isScreen from "../../../../core/screenHelper";
+import { currencySymbolMap } from "./../utils/FeedsTableDataLayer";
 
-        ctx.save();
-        ctx.beginPath();
-        ctx.moveTo(x, topY);
-        ctx.lineTo(x, bottomY);
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "rgba(253, 98, 122, 0.75)";
-        ctx.setLineDash([6, 6]);
-        ctx.stroke();
+const crosshairPlugin = {
+  afterDatasetsDraw: function (chart) {
+    if (chart.tooltip._active && chart.tooltip._active.length) {
+      var activePoint = chart.tooltip._active[0],
+        ctx = chart.ctx,
+        x = activePoint.tooltipPosition().x,
+        y = activePoint.tooltipPosition().y,
+        topY = chart.scales["y-axis-0"].top,
+        bottomY = chart.scales["y-axis-0"].bottom,
+        leftX = chart.scales["x-axis-0"].left,
+        rightX = chart.scales["x-axis-0"].right;
 
-        ctx.beginPath();
-        ctx.moveTo(leftX, y);
-        ctx.lineTo(rightX, y);
-        ctx.stroke();
-        ctx.restore();
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(x, topY);
+      ctx.lineTo(x, bottomY);
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "rgba(253, 98, 122, 0.75)";
+      ctx.setLineDash([6, 6]);
+      ctx.stroke();
 
-        ctx.save();
-        ctx.beginPath();
-        ctx.arc(x, y, 6, 0, 2 * Math.PI);
-        ctx.fillStyle = "rgba(253, 98, 122, 1)";
-        ctx.fill();
-        ctx.restore();
-      }
+      ctx.beginPath();
+      ctx.moveTo(leftX, y);
+      ctx.lineTo(rightX, y);
+      ctx.stroke();
+      ctx.restore();
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.arc(x, y, 6, 0, 2 * Math.PI);
+      ctx.fillStyle = "rgba(253, 98, 122, 1)";
+      ctx.fill();
+      ctx.restore();
+    }
+  },
+};
+
+Chart.plugins.register(crosshairPlugin);
+
+export default {
+  name: "LayerChart",
+  props: {
+    currency: {
+      type: String,
+      required: true,
     },
-  };
-
-  Chart.plugins.register(crosshairPlugin);
-
-  export default {
-    name: "LayerChart",
-    props: {
-      currency: {
-        type: String,
-        required: true,
-      },
-      data: {
-        type: Array,
-        required: true,
-      },
-      range: {
-        type: String,
-        required: true,
-      },
-      duplicatedRanges: {
-        type: Array,
-        required: true,
-      },
-      denomination: {
-        type: String,
-        required: true,
-      },
+    data: {
+      type: Array,
+      required: true,
     },
-    data() {
+    range: {
+      type: String,
+      required: true,
+    },
+    duplicatedRanges: {
+      type: Array,
+      required: true,
+    },
+    denomination: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      chart: null,
+      selectedRange: this.range,
+      ranges: [
+        { value: "1d", label: "1D" },
+        { value: "1w", label: "1W" },
+        { value: "1m", label: "1M" },
+      ],
+      isMobile: false,
+    };
+  },
+  computed: {
+    chartData() {
+      const sortedData = [...this.data]
+        .filter((entry) => {
+          const timestamp = this.parseTimestamp(entry.timestamp);
+          return isValid(timestamp) && !isNaN(parseFloat(entry.value));
+        })
+        .sort((a, b) => {
+          const dateA = this.parseTimestamp(a.timestamp);
+          const dateB = this.parseTimestamp(b.timestamp);
+          return dateA.getTime() - dateB.getTime();
+        });
+
+      const filteredData = this.filterDataByRange(sortedData);
+
       return {
-        chart: null,
-        selectedRange: this.range,
-        ranges: [
-          { value: "1d", label: "1D" },
-          { value: "1w", label: "1W" },
-          { value: "1m", label: "1M" },
+        labels: filteredData.map((entry) => new Date(entry.timestamp)),
+        datasets: [
+          {
+            label: "Price",
+            borderColor: "#FD627A",
+            data: filteredData.map((entry) => ({
+              x: new Date(entry.timestamp),
+              y: parseFloat(entry.value),
+            })),
+            fill: true,
+            lineTension: 0.1,
+            pointRadius: 4,
+            pointHoverRadius: 0,
+          },
         ],
       };
     },
-    computed: {
-      chartData() {
-        const sortedData = [...this.data]
-          .filter((entry) => {
-            const timestamp = this.parseTimestamp(entry.timestamp);
-            return isValid(timestamp) && !isNaN(parseFloat(entry.value));
-          })
-          .sort((a, b) => {
-            const dateA = this.parseTimestamp(a.timestamp);
-            const dateB = this.parseTimestamp(b.timestamp);
-            return dateA.getTime() - dateB.getTime();
-          });
+  },
+  mounted() {
+    this.checkMobileView();
+    window.addEventListener('resize', this.checkMobileView);
+    this.createChart();
+  },
+  methods: {
+    parseTimestamp(timestamp) {
+      if (typeof timestamp === "number") {
+        return new Date(timestamp);
+      } else if (typeof timestamp === "string") {
+        return parseISO(timestamp);
+      }
+      return new Date(NaN);
+    },
+    filterDataByRange(data) {
+      const now = new Date();
+      let startDate;
+      switch (this.selectedRange) {
+        case "1d":
+          startDate = subDays(now, 1);
+          break;
+        case "1w":
+          startDate = subDays(now, 7);
+          break;
+        case "1m":
+          startDate = subMonths(now, 1);
+          break;
+        default:
+          return data;
+      }
+      return data.filter((entry) => {
+        const entryDate = this.parseTimestamp(entry.timestamp);
+        return entryDate >= startDate && entryDate <= now;
+      });
+    },
+    createGradient(ctx, chartArea) {
+      const gradient = ctx.createLinearGradient(
+        0,
+        chartArea.bottom,
+        0,
+        chartArea.top
+      );
+      gradient.addColorStop(0, "rgba(253, 98, 122, 0)");
+      gradient.addColorStop(0.5, "rgba(253, 98, 122, 0.1)");
+      gradient.addColorStop(1, "rgba(253, 98, 122, 0.2)");
+      return gradient;
+    },
+    createChart() {
+      const ctx = this.$refs.chart.getContext("2d");
+      const data = this.chartData;
 
-        const filteredData = this.filterDataByRange(sortedData);
-
+      Chart.Tooltip.positioners.custom = function (elements, position) {
+        if (!elements.length) {
+          return false;
+        }
+        const offset = 20;
+        const x = position.x;
+        const y = position.y;
         return {
-          labels: filteredData.map((entry) => new Date(entry.timestamp)),
-          datasets: [
+          x: x,
+          y: y - offset,
+        };
+      };
+
+      this.chart = new Chart(ctx, {
+        type: "line",
+        data: data,
+        options: this.getChartOptions(),
+      });
+
+      this.$nextTick(() => {
+        this.updateGradient();
+      });
+    },
+    getChartOptions() {
+      const data = this.chartData.datasets[0].data;
+      const minValue = Math.min(...data.map((point) => point.y));
+      const maxValue = Math.max(...data.map((point) => point.y));
+      const range = maxValue - minValue;
+      const padding = range * 0.1;
+
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: { display: false },
+        tooltips: {
+          mode: "index",
+          intersect: false,
+          position: "custom",
+          bodyFontSize: 14,
+          bodyFontStyle: "normal",
+          bodyFontColor: "#404040",
+          backgroundColor: "rgba(254, 254, 254, 1)",
+          borderColor: "#fd627a",
+          borderWidth: 1,
+          titleFontSize: 14,
+          titleFontStyle: "bold",
+          titleFontColor: "#404040",
+          xPadding: 15,
+          yPadding: 15,
+          caretSize: 0,
+          cornerRadius: 15,
+          callbacks: {
+            label: (tooltipItem, data) => {
+              const value = tooltipItem.yLabel;
+              return `  Answer: ${this.currency} ${value.toFixed(5)}`;
+            },
+          },
+        },
+        hover: {
+          mode: "index",
+          intersect: false,
+        },
+        elements: {
+          line: {
+            borderWidth: 1,
+          },
+          point: {
+            radius: this.isMobile ? 0 : 4,
+            hitRadius: this.isMobile ? 0 : 10,
+          },
+        },
+        animation: {
+          duration: 0,
+        },
+        scales: {
+          xAxes: [
             {
-              label: "Price",
-              borderColor: "#FD627A",
-              data: filteredData.map((entry) => ({
-                x: new Date(entry.timestamp),
-                y: parseFloat(entry.value),
-              })),
-              fill: true,
-              lineTension: 0.1,
-              pointRadius: 4,
-              pointHoverRadius: 0,
+              type: "time",
+              time: {
+                unit: this.getTimeUnit(),
+                displayFormats: {
+                  hour: "HH:mm",
+                  day: "MMM D",
+                  week: "MMM D",
+                  month: "MMM YYYY",
+                },
+              },
+              gridLines: {
+                display: false,
+                drawBorder: true,
+                color: "rgba(0, 0, 0, 0.1)",
+              },
+              ticks: {
+                autoSkip: true,
+                maxTicksLimit: 10,
+              },
             },
           ],
-        };
-      },
-    },
-    mounted() {
-      this.createChart();
-    },
-    methods: {
-      parseTimestamp(timestamp) {
-        if (typeof timestamp === "number") {
-          return new Date(timestamp);
-        } else if (typeof timestamp === "string") {
-          return parseISO(timestamp);
-        }
-        return new Date(NaN);
-      },
-      filterDataByRange(data) {
-        const now = new Date();
-        let startDate;
-        switch (this.selectedRange) {
-          case "1d":
-            startDate = subDays(now, 1);
-            break;
-          case "1w":
-            startDate = subDays(now, 7);
-            break;
-          case "1m":
-            startDate = subMonths(now, 1);
-            break;
-          default:
-            return data;
-        }
-        return data.filter((entry) => {
-          const entryDate = this.parseTimestamp(entry.timestamp);
-          return entryDate >= startDate && entryDate <= now;
-        });
-      },
-      createGradient(ctx, chartArea) {
-        const gradient = ctx.createLinearGradient(
-          0,
-          chartArea.bottom,
-          0,
-          chartArea.top
-        );
-        gradient.addColorStop(0, "rgba(253, 98, 122, 0)");
-        gradient.addColorStop(0.5, "rgba(253, 98, 122, 0.1)");
-        gradient.addColorStop(1, "rgba(253, 98, 122, 0.2)");
-        return gradient;
-      },
-      createChart() {
-        const ctx = this.$refs.chart.getContext("2d");
-        const data = this.chartData;
-
-        Chart.Tooltip.positioners.custom = function (elements, position) {
-          if (!elements.length) {
-            return false;
-          }
-          const offset = 20;
-          const x = position.x;
-          const y = position.y;
-          return {
-            x: x,
-            y: y - offset,
-          };
-        };
-
-        this.chart = new Chart(ctx, {
-          type: "line",
-          data: data,
-          options: this.getChartOptions(),
-        });
-
-        this.$nextTick(() => {
-          this.updateGradient();
-        });
-      },
-      getChartOptions() {
-        const data = this.chartData.datasets[0].data;
-        const minValue = Math.min(...data.map((point) => point.y));
-        const maxValue = Math.max(...data.map((point) => point.y));
-        const range = maxValue - minValue;
-        const padding = range * 0.1;
-
-        return {
-          responsive: true,
-          maintainAspectRatio: false,
-          legend: { display: false },
-          tooltips: {
-            mode: "index",
-            intersect: false,
-            position: "custom",
-            // Styling
-            bodyFontSize: 14,
-            bodyFontStyle: "normal",
-            bodyFontColor: "#404040",
-            backgroundColor: "rgba(254, 254, 254, 1)",
-            borderColor: "#fd627a",
-            borderWidth: 1,
-            titleFontSize: 14,
-            titleFontStyle: "bold",
-            titleFontColor: "#404040",
-            xPadding: 15,
-            yPadding: 15,
-            caretSize: 0,
-            cornerRadius: 15,
-            // End styling
-            callbacks: {
-              label: (tooltipItem, data) => {
-                const value = tooltipItem.yLabel;
-                return `  Answer: ${this.currency} ${value.toFixed(5)}`;
-              },
-            },
-          },
-          hover: {
-            mode: "index",
-            intersect: false,
-          },
-          elements: {
-            line: {
-              borderWidth: 1,
-            },
-            point: {
-              radius: 0,
-              hitRadius: 10,
-            },
-          },
-          animation: {
-            duration: 0,
-          },
-          scales: {
-            xAxes: [
-              {
-                type: "time",
-                time: {
-                  unit: this.getTimeUnit(),
-                  displayFormats: {
-                    hour: "HH:mm",
-                    day: "MMM D",
-                    week: "MMM D",
-                    month: "MMM YYYY",
-                  },
-                },
-                gridLines: {
-                  display: false,
-                  drawBorder: true,
-                  color: "rgba(0, 0, 0, 0.1)",
-                },
-                ticks: {
-                  autoSkip: true,
-                  maxTicksLimit: 10,
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false,
+                suggestedMin: minValue - padding,
+                suggestedMax: maxValue + padding,
+                callback: (value) => {
+                  return `${currencySymbolMap[this.denomination] || this.denomination} ${value.toFixed(2)}`;
                 },
               },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: false,
-                  suggestedMin: minValue - padding,
-                  suggestedMax: maxValue + padding,
-                  callback: (value) => {
-                    return `${currencySymbolMap[this.denomination] || this.denomination} ${value.toFixed(2)}`;
-                  },
-                },
-              },
-            ],
-          },
-          layout: {
-            padding: {
-              right: 20,
             },
-          },
-        };
-      },
-      updateGradient() {
-        if (this.chart && this.chart.chartArea) {
-          const chartArea = this.chart.chartArea;
-          const ctx = this.chart.ctx;
-          const gradient = this.createGradient(ctx, chartArea);
-          this.chart.data.datasets[0].backgroundColor = gradient;
-          this.chart.update();
-        }
-      },
-      getTimeUnit() {
-        switch (this.selectedRange) {
-          case "1d":
-            return "hour";
-          case "1w":
-            return "day";
-          case "1m":
-            return "day";
-          default:
-            return "day";
-        }
-      },
-      updateChart() {
-        if (this.chart) {
-          const newData = this.chartData;
-          this.chart.data.labels = newData.labels;
-          this.chart.data.datasets[0].data = newData.datasets[0].data;
-          this.chart.options = this.getChartOptions();
-          this.updateGradient();
-          this.chart.update({
-            duration: 0,
-            resetTransforms: true,
-          });
-        }
-      },
-      onRangeChange(range) {
-        this.$emit("range-change", range);
-      },
-    },
-    watch: {
-      data: {
-        handler: "updateChart",
-        deep: true,
-      },
-      range: {
-        handler(newRange) {
-          this.selectedRange = newRange;
-          this.updateChart();
+          ],
         },
-      },
+        layout: {
+          padding: {
+            right: 20,
+          },
+        },
+      };
     },
-    beforeDestroy() {
-      if (this.chart) {
-        this.chart.destroy();
+    updateGradient() {
+      if (this.chart && this.chart.chartArea) {
+        const chartArea = this.chart.chartArea;
+        const ctx = this.chart.ctx;
+        const gradient = this.createGradient(ctx, chartArea);
+        this.chart.data.datasets[0].backgroundColor = gradient;
+        this.chart.update();
       }
     },
-  };
+    getTimeUnit() {
+      switch (this.selectedRange) {
+        case "1d":
+          return "hour";
+        case "1w":
+          return "day";
+        case "1m":
+          return "day";
+        default:
+          return "day";
+      }
+    },
+    updateChart() {
+      if (this.chart) {
+        const newData = this.chartData;
+        this.chart.data.labels = newData.labels;
+        this.chart.data.datasets[0].data = newData.datasets[0].data;
+        this.chart.options = this.getChartOptions();
+        this.updateGradient();
+        this.chart.update({
+          duration: 0,
+          resetTransforms: true,
+        });
+      }
+    },
+    onRangeChange(range) {
+      this.$emit("range-change", range);
+    },
+    checkMobileView() {
+      this.isMobile = isScreen('xs') || isScreen('sm');
+      if (this.chart) {
+        this.updateChart();
+      }
+    },
+  },
+  watch: {
+    data: {
+      handler: "updateChart",
+      deep: true,
+    },
+    range: {
+      handler(newRange) {
+        this.selectedRange = newRange;
+        this.updateChart();
+      },
+    },
+  },
+  beforeDestroy() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+    window.removeEventListener('resize', this.checkMobileView);
+  },
+};
 </script>
 
 <style scoped lang="scss">
