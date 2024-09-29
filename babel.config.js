34b73f5a708af5e0bb5d7153a590184254b9e55a
metter <139591245+metter0@users.noreply.github.com>
2024-09-29 19:38:17 +0500

    Update babel.config.js
    Plugins have been added here to support zero-merge and dynamic imports. Also, settings have been improved to reduce the size of the output code.

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,10 @@ module.exports = {
     '@vue/app'
   ],
   plugins: [
-    "@babel/plugin-proposal-optional-chaining"
-  ]
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-syntax-dynamic-import'
+  ],
+  comments: false, // убирает комментарии из скомпилированного кода
+  compact: true    // уменьшает размер выходного кода
 };
